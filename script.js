daaa40a028e2999ac963b8679b5fdb49ecc630e8
Elba <elba.duishekeeva16@gmail.com>
2023-11-16 23:48:27 +0600

    cocktails1

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-const row=document.querySelector('.row')
-const all=document.querySelector('#all')
+const picture = document.querySelector('.img-box')
+const all = document.querySelector('#all')
 const search = document.querySelector('#search')
-const submit=document.querySelector('#submit')
-const searchInput=document.querySelector('#searchInput')
-const name=document.querySelector('#name')
-const category=document.querySelector('#category')
-const instruction=document.querySelector('#instruction')
-const img=document.querySelector('#img')
-const ingredients=document.querySelector('#ingredients')
-const result=document.querySelector('.result')
+const submit = document.querySelector('#submit')
+const searchInput = document.querySelector('#searchInput')
+const name = document.querySelector('#name')
+const category = document.querySelector('#category')
+const instruction = document.querySelector('#instruction')
+const img = document.querySelector('#img')
+const ingredients = document.querySelector('#ingredients')
+const result = document.querySelector('.result')
+const searchWrapper=document.querySelector('.search-wrapper')
 
 
-
-const handleDrink = ()=> {
+const handleDrink = () => {
     fetch('https://www.thecocktaildb.com/api/json/v2/1/popular.php')
         .then(res => res.json())
         .then(json => {
             console.log(json)
             json.drinks.forEach(drinks => {
-                row.innerHTML +=`
+                picture.innerHTML += `
                             <div class="col-4">
                 <div class="card">
                     <img src="${drinks.strDrinkThumb}" class="card-img-top" alt=""/>
                     <div class="card-body">
-                    <h2 class="card-title">${drinks.strDrink}</h2>
+                    <h2  class="card-title">${drinks.strDrink}</h2>
                     <p class="card-text">${drinks.strCategory}</p>
                     <p class="">${drinks.strAlcoholic}</p>
                     </div>
@@ -32,35 +32,58 @@ const handleDrink = ()=> {
                 `
             })
         })
+
 }
 handleDrink()
 
 all.addEventListener('change', () => {
     if (all.checked) {
-        row.classList.remove('hidden')
+        picture.classList.remove('hidden')
         search.classList.add('hidden')
+        searchWrapper.classList.add('hidden')
     }
 })
 
 search.addEventListener('change', () => {
     if (search.checked) {
         search.classList.remove('hidden')
-        row.classList.add('hidden')
+        picture.classList.add('hidden')
+        searchWrapper.classList.remove('hidden')
     }
 })
 
-const handleSearch=()=> {
-    let value=searchInput.value
+const handleSearch = () => {
+    let value = searchInput.value
     result.classList.remove('hidden')
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${value}`)
         .then(res => res.json())
         .then(json => {
             console.log(value)
+            const cocktail = json.drinks[0]
+            ingredients.innerHTML=`
+            <div class="col-4">
+                <div>
+                    <img src="https://www.thecocktaildb.com/images/ingredients/${cocktail.strIngredient1}-Medium.png" alt="">
+                    <p>${cocktail.strIngredient1}</p>
+                </div>
+            </div>
+            <div class="col-4">
+                <div>
+                    <img src="https://www.thecocktaildb.com/images/ingredients/${cocktail.strIngredient2}-Medium.png" alt="">
+                    <p>${cocktail.strIngredient2}</p>
+                </div>
+            </div>
+            <div class="col-4">
+            <img src="https://www.thecocktaildb.com/images/ingredients/${cocktail.strIngredient3}-Medium.png" alt="">
+            <p>${cocktail.strIngredient3}</p>
+            </div>
+           
+            `
             json.drinks.forEach(drinks => {
-                img.src=drinks.strDrinkThumb
-                name.innerHTML='Name: '+ drinks.strDrink
-                category.innerText='Category: '+drinks.strCategory
-                instruction.innerText='Instruction: '+drinks.strInstructions
+                img.src = drinks.strDrinkThumb
+                name.innerHTML =drinks.strDrink
+                category.innerText = 'Category: ' + drinks.strCategory
+                instruction.innerText = drinks.strInstructions
             })
 
         })
